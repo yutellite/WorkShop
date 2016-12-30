1671cbf8d78eedae7f33b83a04d31feacec2fdbc
yutellite <yutellite@users.noreply.github.com>
2016-12-30 11:26:31 +0800

    Update avl_tree.c
    [DTS SN]:NA
    [Modiry]: 1. Rename the name of unction to normalize; 2. add assert
    [Verify]:

--- avl_tree.c
+++ avl_tree.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #ifndef bool
 typedef int bool;
@@ -8,10 +9,12 @@ typedef int bool;
 #define true 1
 #endif
 
-int max(int a, int b)
-{
-    return (a>b)?a:b;
-}
+#ifdef DEBUG
+    #define ASSERT(p) assert(p)
+#else
+    #define ASSERT(p) (void*)(p)
+#endif
+
 typedef struct TreeNode
 {
     int key;
@@ -21,17 +24,22 @@ typedef struct TreeNode
     struct TreeNode* right;
 }TreeNode;
 
-void destroy(TreeNode**tree)
+int max(int a, int b)
+{
+    return (a>b)?a:b;
+}
+
+void avl_destroy(TreeNode**tree)
 {
     if(*tree==NULL)
         return;
-    destroy(&(*tree)->left);
-    destroy(&(*tree)->right);
+    avl_destroy(&(*tree)->left);
+    avl_destroy(&(*tree)->right);
     printf("%d ",(*tree)->key);
     free(*tree);
     *tree=NULL;
 }
-TreeNode* newnode(int key)
+TreeNode* avl_newnode(int key)
 {
     TreeNode* node=(TreeNode*)malloc(sizeof(TreeNode));
     node->key=key;
@@ -42,7 +50,7 @@ TreeNode* newnode(int key)
     return(node);
 }
 
-void insert(TreeNode** tree, int val)
+void avl_insert(TreeNode** tree, int val)
 {
     TreeNode* temp=NULL;
     if(*tree==NULL)
@@ -55,127 +63,27 @@ void insert(TreeNode** tree, int val)
     }
     
     if(val<(*tree)->key)
-        insert(&(*tree)->left,val);
+        avl_insert(&(*tree)->left,val);
     else if(val>(*tree)->key)
-        insert(&(*tree)->right,val);
+        avl_insert(&(*tree)->right,val);
     else
-        printf("Err. Cannot insert a duplicate key %d.\n",val);
-}
-void preOrder(TreeNode* tree)
-{
-    if(tree==NULL)
-        return;
-    printf("%d ",tree->key);
-    if(tree->left!=NULL)
-        preOrder(tree->left);
-    if(tree->right!=NULL)
-        preOrder(tree->right);
-}
-void midOrder(TreeNode* tree)
-{
-    if(tree==NULL)
-        return;
-    if(tree->left!=NULL)
-        midOrder(tree->left);    
-    printf("%d ",tree->key);
-    if(tree->right!=NULL)
-        midOrder(tree->right);
-}
-void postOrder(TreeNode* tree)
-{
-    if(tree==NULL)
-        return;
-    if(tree->left!=NULL)
-        postOrder(tree->left);
-    if(tree->right!=NULL)
-        postOrder(tree->right);        
-    printf("%d ",tree->key);
+        printf("Err. Cannot avl_insert a duplicate key %d.\n",val);
 }
 
-int height(TreeNode* N)
+int avl_height(TreeNode* N)
 {
     if(N==NULL)
         return 0;
     return N->height;
 }
-/* get the height by traversing the tree nodes
- *
-*/
-int MaxDepth(TreeNode *root)
+
+int avl_getbalance(TreeNode* t)
 {
-    int ldepth = 0;
-    int rdepth = 0;
-    if(root == NULL)
+    if(t==NULL)
         return 0;
-    ldepth = MaxDepth(root->left);
-    rdepth = MaxDepth(root->right);
-    return (ldepth > rdepth ? ldepth : rdepth) + 1;   
-}
-/*
- *you can get an inverse tree if you travese the tree inorder and the tree is a balaceed tree.
-*/
-void mirror(TreeNode*root)
-{
-    TreeNode *temp=NULL;
-    if(root==NULL)
-        return;
-    mirror(root->left);
-    mirror(root->right);
-    temp=root->left;
-    root->left=root->right;
-    root->right=temp;
-}
-void printgivenlevel(TreeNode* T, int level)
-{
-    if(T==NULL)
-    {
-        printf("null ");
-        return;
-    }
-    if(level==1)
-        printf("%d ",T->key);
-    else
-    {
-        printgivenlevel(T->left,level-1);
-        printgivenlevel(T->right,level-1);
-    }
+    return avl_height(t->left)-avl_height(t->right);
 }
 
-void levelOrder(TreeNode* T)
-{
-    int h=height(T);
-    int i=0;
-    for(i=1;i<=h;i++)
-    {
-        printgivenlevel(T,i);
-        printf("\n");
-    }
-}
-
-int countNode(TreeNode* root)
-{
-    int count=0;
-    if(root==NULL)
-        return 0;
-    count+=countNode(root->left);
-    count+=countNode(root->right);    
-    count+=1;
-    return count;
-}
-
-bool isbalanced(TreeNode* root)
-{
-    int ldepth=0;
-    int rdepth=0;
-    if(root==NULL)
-        return true;
-    ldepth=MaxDepth(root->left);
-    rdepth=MaxDepth(root->right);
-    if(abs(ldepth-rdepth)<=1&&true==isbalanced(root->left)&&true==isbalanced(root->right))
-        return true;
-    else 
-        return false;
-}
 /*
   z                                y
  /  \                            /   \ 
@@ -185,8 +93,9 @@ T1   y     Left Rotate(z)       z      x
        / \
      T3orT4
 */
-TreeNode* leftrotate(TreeNode* z)
+TreeNode* avl_leftrotate(TreeNode* z)
 {
+    ASSERT(z);
     TreeNode* y=z->right;
     TreeNode* t2=y->left;
     
@@ -194,8 +103,8 @@ TreeNode* leftrotate(TreeNode* z)
     y->left=z;
     z->right=t2;
     
-    z->height=max(height(z->left),height(z->right))+1;
-    y->height=max(height(y->left),height(y->right))+1;
+    z->height=max(avl_height(z->left),avl_height(z->right))+1;
+    y->height=max(avl_height(y->left),avl_height(y->right))+1;
     return y;
 }
 /*
@@ -207,8 +116,10 @@ TreeNode* leftrotate(TreeNode* z)
     / \
   T1 orT2  
 */
-TreeNode* rightrotate(TreeNode*z)
+
+TreeNode* avl_rightrotate(TreeNode* z)
 {
+    ASSERT(z);
     TreeNode* y=z->left;
     TreeNode* t3=y->right;
     
@@ -216,62 +127,11 @@ TreeNode* rightrotate(TreeNode*z)
     y->right=z;
     z->left=t3;
     
-    z->height=max(height(z->left),height(z->right))+1;    
-    y->height=max(height(y->left),height(y->right))+1;
+    z->height=max(avl_height(z->left),avl_height(z->right))+1;    
+    y->height=max(avl_height(y->left),avl_height(y->right))+1;
     return y;
 }
 
-int getbalance(TreeNode* t)
-{
-    if(t==NULL)
-        return 0;
-    return height(t->left)-height(t->right);
-}
-
-TreeNode* insertbalance(TreeNode* node, int key)
-{
-    //1.perform the normal BST insertion
-    if(node==NULL)
-        return newnode(key);
-    if(key<node->key)
-        node->left=insertbalance(node->left,key);
-    else if(key>node->key)
-        node->right=insertbalance(node->right,key);
-    else
-    {
-        printf("Err. Cannot insert a duplicate key %d.\n",key);
-        return node;
-    }
-    //2.update height of this ancestor node
-    node->height=1+max(height(node->left),height(node->right));
-    
-    //3.get the balace factor of this ancestor node to check whether this node became unbalanced.
-    int balance=getbalance(node);
-    
-    //4.if this node becomes unbalanced, then there are 4 cases
-    
-    //left left case
-    if(balance>1 && key<node->left->key)
-        return rightrotate(node);
-    //left right case
-    if(balance>1 && key>node->left->key)
-    {
-        node->left=leftrotate(node->left);
-        return rightrotate(node);
-        
-    }    
-    //right right case
-    if(balance<-1 && key>node->right->key)
-        return leftrotate(node);
-    //right left case
-    if(balance<-1 && key<node->right->key)
-    {
-        node->right=rightrotate(node->right);
-        return leftrotate(node);
-    }
-
-    return node;
-}
 /*
 a) Left Left Case
 
@@ -311,80 +171,235 @@ T1   y   Right Rotate (y)    T1   x      Left Rotate(z)   z      y
   / \                              /  \
 T2 orT3                           T3   T4
 */
+TreeNode* avl_insertbalance(TreeNode* node, int key)
+{
+    //1.perform the normal BST avl_insertion
+    if(node==NULL)
+        return avl_newnode(key);
+    if(key<node->key)
+        node->left=avl_insertbalance(node->left,key);
+    else if(key>node->key)
+        node->right=avl_insertbalance(node->right,key);
+    else
+    {
+        printf("Err. Cannot avl_insert a duplicate key %d.\n",key);
+        return node;
+    }
+    //2.update height of this ancestor node
+    node->height=1+max(avl_height(node->left),avl_height(node->right));
+    
+    //3.get the balace factor of this ancestor node to check whether this node became unbalanced.
+    int balance=avl_getbalance(node);
+    
+    //4.if this node becomes unbalanced, then there are 4 cases
+    
+    //left left case
+    if(balance>1 && key<node->left->key)
+        return avl_rightrotate(node);
+    //left right case
+    if(balance>1 && key>node->left->key)
+    {
+        node->left=avl_leftrotate(node->left);
+        return avl_rightrotate(node);
+        
+    }    
+    //right right case
+    if(balance<-1 && key>node->right->key)
+        return avl_leftrotate(node);
+    //right left case
+    if(balance<-1 && key<node->right->key)
+    {
+        node->right=avl_rightrotate(node->right);
+        return avl_leftrotate(node);
+    }
+
+    return node;
+}
+void avl_printgivenlevel(TreeNode* T, int level)
+{
+    if(T==NULL)
+    {
+        printf("null ");
+        return;
+    }
+    if(level==1)
+        printf("%d ",T->key);
+    else
+    {
+        avl_printgivenlevel(T->left,level-1);
+        avl_printgivenlevel(T->right,level-1);
+    }
+}
+void avl_preOrder(TreeNode* tree)
+{
+    if(tree==NULL)
+        return;
+    printf("%d ",tree->key);
+    if(tree->left!=NULL)
+        avl_preOrder(tree->left);
+    if(tree->right!=NULL)
+        avl_preOrder(tree->right);
+}
+void avl_midOrder(TreeNode* tree)
+{
+    if(tree==NULL)
+        return;
+    if(tree->left!=NULL)
+        avl_midOrder(tree->left);    
+    printf("%d ",tree->key);
+    if(tree->right!=NULL)
+        avl_midOrder(tree->right);
+}
+void avl_postOrder(TreeNode* tree)
+{
+    if(tree==NULL)
+        return;
+    if(tree->left!=NULL)
+        avl_postOrder(tree->left);
+    if(tree->right!=NULL)
+        avl_postOrder(tree->right);        
+    printf("%d ",tree->key);
+}
+
+void avl_levelOrder(TreeNode* T)
+{
+    if(T==NULL)
+        return;
+    int h=avl_height(T);
+    int i=0;
+    for(i=1;i<=h;i++)
+    {
+        avl_printgivenlevel(T,i);
+        printf("\n");
+    }
+}
+/* get the height by traversing the tree nodes
+ *
+*/
+int avl_MaxDepth(TreeNode *root)
+{
+    int ldepth = 0;
+    int rdepth = 0;
+    if(root == NULL)
+        return 0;
+    ldepth = avl_MaxDepth(root->left);
+    rdepth = avl_MaxDepth(root->right);
+    return (ldepth > rdepth ? ldepth : rdepth) + 1;   
+}
+/*
+ *you can get an inverse tree if you travese the tree inorder and the tree is a balaceed tree.
+*/
+void avl_mirror(TreeNode*root)
+{
+    TreeNode *temp=NULL;
+    if(root==NULL)
+        return;
+    avl_mirror(root->left);
+    avl_mirror(root->right);
+    temp=root->left;
+    root->left=root->right;
+    root->right=temp;
+}
+
+
+int avl_countnode(TreeNode* root)
+{
+    int count=0;
+    if(root==NULL)
+        return 0;
+    count+=avl_countnode(root->left);
+    count+=avl_countnode(root->right);    
+    count+=1;
+    return count;
+}
+
+bool avl_isbalanced(TreeNode* root)
+{
+    int ldepth=0;
+    int rdepth=0;
+    if(root==NULL)
+        return true;
+    ldepth=avl_MaxDepth(root->left);
+    rdepth=avl_MaxDepth(root->right);
+    if(abs(ldepth-rdepth)<=1&&true==avl_isbalanced(root->left)&&true==avl_isbalanced(root->right))
+        return true;
+    else 
+        return false;
+}
 int main()
 {   
     int depth=0;
     TreeNode* root=NULL; 
     TreeNode* rootbalance=NULL;    
-    insert(&root,0);
-    insert(&root,5);
-    insert(&root,8);
-    insert(&root,2);
-    insert(&root,4);
-    insert(&root,5);
-    insert(&root,1);
-    insert(&root,2);
-    insert(&root,9);
-    insert(&root,7);
-    insert(&root,6);
-    insert(&root,12);
-    insert(&root,4);
-    insert(&root,8);
-    preOrder(root);
+    avl_insert(&root,0);
+    avl_insert(&root,5);
+    avl_insert(&root,8);
+    avl_insert(&root,2);
+    avl_insert(&root,4);
+    avl_insert(&root,5);
+    avl_insert(&root,1);
+    avl_insert(&root,2);
+    avl_insert(&root,9);
+    avl_insert(&root,7);
+    avl_insert(&root,6);
+    avl_insert(&root,12);
+    avl_insert(&root,4);
+    avl_insert(&root,8);
+    avl_preOrder(root);
     printf("\n");
     
-    midOrder(root);    
+    avl_midOrder(root);    
     printf("\n");
     
-    postOrder(root);    
+    avl_postOrder(root);    
     printf("\n");
     
     printf("before delete\n");
-    printf("the depth is %d\n",MaxDepth(root));
-    printf("the ldepth is %d\n",MaxDepth(root->left));
-    printf("the rdepth is %d\n",MaxDepth(root->right));
-    printf("the count node is %d\n",countNode(root));
-    printf("the tree is %sbalanced.\n",isbalanced(root)?"":"not ");
-    mirror(root);
+    printf("the depth is %d\n",avl_MaxDepth(root));
+    printf("the ldepth is %d\n",avl_MaxDepth(root->left));
+    printf("the rdepth is %d\n",avl_MaxDepth(root->right));
+    printf("the count node is %d\n",avl_countnode(root));
+    printf("the tree is %sbalanced.\n",avl_isbalanced(root)?"":"not ");
+    avl_mirror(root);
     printf("mirror tree\n");
-    preOrder(root);
+    avl_preOrder(root);
     printf("\n");
     
-    midOrder(root);    
+    avl_midOrder(root);    
     printf("\n");
     
-    postOrder(root);    
+    avl_postOrder(root);    
     printf("\n");
 //    deleteall(&root);  
-    destroy(&root);  
+    avl_destroy(&root);  
     printf("\n");
     
     printf("after delete\n");
-    midOrder(root);    
+    avl_midOrder(root);    
     printf("\n");
-    printf("the depth is %d\n",MaxDepth(root));
-    printf("the count node is %d\n",countNode(root));
+    printf("the depth is %d\n",avl_MaxDepth(root));
+    printf("the count node is %d\n",avl_countnode(root));
     
-    rootbalance=insertbalance(rootbalance,1);
-    rootbalance=insertbalance(rootbalance,9);
-    rootbalance=insertbalance(rootbalance,23);
-    rootbalance=insertbalance(rootbalance,5);
-    rootbalance=insertbalance(rootbalance,8);
-    rootbalance=insertbalance(rootbalance,6);
-    rootbalance=insertbalance(rootbalance,2);
-    rootbalance=insertbalance(rootbalance,4);
-    rootbalance=insertbalance(rootbalance,10);
-    rootbalance=insertbalance(rootbalance,11);
-    rootbalance=insertbalance(rootbalance,12);
-    rootbalance=insertbalance(rootbalance,7);
-    rootbalance=insertbalance(rootbalance,15);    
-    rootbalance=insertbalance(rootbalance,5);
-    rootbalance=insertbalance(rootbalance,11);
-    rootbalance=insertbalance(rootbalance,23);
-    rootbalance=insertbalance(rootbalance,18);
-    rootbalance=insertbalance(rootbalance,19);
-    printf("the tree is %sbalanced.\n",isbalanced(rootbalance)?"":"not ");
-    levelOrder(rootbalance);
+    rootbalance=avl_insertbalance(rootbalance,1);
+    rootbalance=avl_insertbalance(rootbalance,9);
+    rootbalance=avl_insertbalance(rootbalance,23);
+    rootbalance=avl_insertbalance(rootbalance,5);
+    rootbalance=avl_insertbalance(rootbalance,8);
+    rootbalance=avl_insertbalance(rootbalance,6);
+    rootbalance=avl_insertbalance(rootbalance,2);
+    rootbalance=avl_insertbalance(rootbalance,4);
+    rootbalance=avl_insertbalance(rootbalance,10);
+    rootbalance=avl_insertbalance(rootbalance,11);
+    rootbalance=avl_insertbalance(rootbalance,12);
+    rootbalance=avl_insertbalance(rootbalance,7);
+    rootbalance=avl_insertbalance(rootbalance,15);    
+    rootbalance=avl_insertbalance(rootbalance,5);
+    rootbalance=avl_insertbalance(rootbalance,11);
+    rootbalance=avl_insertbalance(rootbalance,23);
+    rootbalance=avl_insertbalance(rootbalance,18);
+    rootbalance=avl_insertbalance(rootbalance,19);
+    printf("the tree is %sbalanced.\n",avl_isbalanced(rootbalance)?"":"not ");
+    avl_levelOrder(rootbalance);
     printf("\n");
     return 0;
 }
